0ca3082dc517ca418c77a0806ff66fc25d0bab1a
Glauber Ferreira <glauber.ferreira@ifal.edu.br>
2024-07-27 11:55:52 -0300

    Versão inicial da primeira tela. Volta versão do RNP (bug no TextInput)

--- app/index.js
+++ app/index.js
@@ -1,10 +1,23 @@
+import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Button, TextInput } from 'react-native-paper';
 
 export default function App() {
+  const [email, setEmail] = useState('glauber.ferreira@ifal.edu.br')
+  const [senha, setSenha] = useState('')
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput label="E-mail" value={email} onChangeText={setEmail} placeholder='E-mail'/>
+      <TextInput label="Senha" value={senha} onChangeText={setSenha} placeholder='Senha' secureTextEntry/>
+      <Button mode='contained'>Login</Button>
+      <Button mode='contained'>Login com o Google</Button>
+      <View style={styles.links}>
+        <Link href='/'>Cadastrar</Link>
+        <Link href='/'>Esqueci a Senha</Link>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +27,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
+  links: {
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  }
 });
