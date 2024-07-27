8321035ce41412088a6a026fa613079411d06450
Glauber Ferreira <glauber.ferreira@ifal.edu.br>
2024-07-27 12:00:23 -0300

    Firebase - Initialize the SDK in your project
    https://docs.expo.dev/guides/using-firebase/#install-the-sdk

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBpeIiYt3BSKBrhRAOf11_FzNf55AFgPZ0",
+  authDomain: "programacao-movel-tarde.firebaseapp.com",
+  projectId: "programacao-movel-tarde",
+  storageBucket: "programacao-movel-tarde.appspot.com",
+  messagingSenderId: "727603664468",
+  appId: "1:727603664468:web:663e3c4435600dce34bfc1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
