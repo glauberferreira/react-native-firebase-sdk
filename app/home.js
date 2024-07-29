05357f83ce13b16c4b2f7f145d9b75b79bd0386d
Glauber Ferreira <glauber.ferreira@ifal.edu.br>
2024-07-29 10:50:57 -0300

    Implementa verificação de e-mail e user.emailVerified.toString()

--- app/home.js
+++ app/home.js
@@ -1,12 +1,25 @@
 import { View, Text } from 'react-native'
 import { Button } from 'react-native-paper';
 import { auth } from '../firebaseConfig';
-import { signOut } from "firebase/auth";
+import { sendEmailVerification, signOut } from "firebase/auth";
 import { useRouter } from 'expo-router';
+import { useState } from 'react';
 
 const Home = () => {
+  const [loading, setLoading] = useState(false);
   const router = useRouter();
   const user = auth.currentUser;
+
+  const handleVerificarEmail = async () => {
+    try {
+      setLoading(true);
+      await sendEmailVerification(auth.currentUser);
+      setLoading(false);
+    } catch (error) {
+      console.error(error.code);
+      console.error(error.message);      
+    }
+  }
 
   const handleSair = async () => {
     try {
@@ -23,9 +36,10 @@ const Home = () => {
       <Text>UID: {user.uid}</Text>
       <Text>Display Name: {user.displayName}</Text>
       <Text>E-mail: {user.email}</Text>
-      <Text>E-mail Verified: {user.emailVerified}</Text>
+      <Text>E-mail Verified: {user.emailVerified.toString()}</Text>
       <Text>Phone Number: {user.phoneNumber}</Text>
       <Text>Photo URL: {user.photoURL}</Text>
+      <Button mode='contained' onPress={handleVerificarEmail} loading={loading} disabled={user.emailVerified}>Verificar E-mail</Button>
       <Button mode='contained' onPress={handleSair}>Sair</Button>
     </View>
   )
