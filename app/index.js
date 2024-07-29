80184343c927d483f153b6af13a4a7469e1eefa4
Glauber Ferreira <glauber.ferreira@ifal.edu.br>
2024-07-28 21:10:33 -0300

    Firebase Auth - Sign in a user with an email address and password
    https://firebase.google.com/docs/auth/web/password-auth#sign_in_a_user_with_an_email_address_and_password

--- app/index.js
+++ app/index.js
@@ -3,16 +3,35 @@ import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Button, TextInput } from 'react-native-paper';
+import { auth } from '../firebaseConfig';
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 export default function App() {
   const [email, setEmail] = useState('glauber.ferreira@ifal.edu.br')
   const [senha, setSenha] = useState('')
 
+  const handleLogin = () => {
+    signInWithEmailAndPassword(auth, email, senha)
+    .then((userCredential) => {
+      // Signed up 
+      const user = userCredential.user;
+      console.log(user);
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.error(errorCode);
+      console.error(errorMessage);
+      // ..
+    });    
+  }
+
   return (
     <View style={styles.container}>
       <TextInput label="E-mail" value={email} onChangeText={setEmail} placeholder='E-mail'/>
       <TextInput label="Senha" value={senha} onChangeText={setSenha} placeholder='Senha' secureTextEntry/>
-      <Button mode='contained'>Login</Button>
+      <Button mode='contained' onPress={handleLogin}>Login</Button>
       <Button mode='contained'>Login com o Google</Button>
       <View style={styles.links}>
         <Link href='/'>Cadastrar</Link>
