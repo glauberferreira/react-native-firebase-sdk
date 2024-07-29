fa87aa95b2801dbe05c65d59499525b9dd2739bf
Glauber Ferreira <glauber.ferreira@ifal.edu.br>
2024-07-28 21:29:32 -0300

    Implementa signOut

--- app/home.js
+++ app/home.js
@@ -1,9 +1,26 @@
 import { View, Text } from 'react-native'
+import { Button } from 'react-native-paper';
+import { auth } from '../firebaseConfig';
+import { signOut } from "firebase/auth";
+import { useRouter } from 'expo-router';
 
 const Home = () => {
+  const router = useRouter();
+
+  const handleSair = async () => {
+    try {
+      await signOut(auth);
+      router.replace('/');
+    } catch (error) {
+      console.error(error.code);
+      console.error(error.message);
+    }
+  }
+
   return (
     <View>
       <Text>Home</Text>
+      <Button mode='contained' onPress={handleSair}>Sair</Button>
     </View>
   )
 }
