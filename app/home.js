4f3cbf134f1bb120fc2f38cdea54834a31628086
Glauber Ferreira <glauber.ferreira@ifal.edu.br>
2024-08-19 13:35:26 -0300

    Faz download de uma imagem a partir do Cloud Storage

--- app/home.js
+++ app/home.js
@@ -1,4 +1,4 @@
-import { View, Text, ActivityIndicator, StyleSheet } from 'react-native'
+import { View, Text, ActivityIndicator, StyleSheet, Image } from 'react-native'
 import { Button } from 'react-native-paper';
 import { auth, db } from '../firebaseConfig';
 import { sendEmailVerification, signOut } from "firebase/auth";
@@ -6,13 +6,16 @@ import { Link, useRouter } from 'expo-router';
 import { useEffect, useState } from 'react';
 import Estilo from './estilo';
 import { doc, getDoc } from "firebase/firestore";
+import { getDownloadURL, getStorage, ref } from "firebase/storage";
 
 const Home = () => {
   const [loadingTela, setLoadingTela] = useState(true);
   const [loadingVerificarEmail, setLoadingVerificarEmail] = useState(false);
   const [usuarioFirestore, setUsuarioFirestore] = useState({});
+  const [url, setUrl] = useState('');
   const router = useRouter();
   const user = auth.currentUser;
+  const storage = getStorage();
 
   const getUsuarioFirestore = async () => {
     try {
@@ -31,8 +34,18 @@ const Home = () => {
     }
   }
 
+  const getImagem = async () => {
+    try {
+      const url = await getDownloadURL(ref(storage, 'banner.jpeg'));
+      setUrl(url);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   useEffect(() => {
     getUsuarioFirestore();
+    getImagem();
   }, []);
 
   const handleVerificarEmail = async () => {
@@ -75,6 +88,7 @@ const Home = () => {
           </View>
           <Button mode='contained' onPress={handleVerificarEmail} loading={loadingVerificarEmail} disabled={user.emailVerified}>Verificar E-mail</Button>
           <Button mode='contained' onPress={handleSair}>Sair</Button>
+          <Image source={{uri: url}} style={{width: 312, height: 211}}/>
         </>
       )}
     </View>
